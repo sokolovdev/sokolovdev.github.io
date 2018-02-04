6807d96f63d80fa203920d2a8af4c324080d153a
unknown <naraxiss@gmail.com>
2018-02-04 16:21:31 +0300

    ok

--- _js/core.js
+++ _js/core.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-	
-})
+	$('.header__hamburger').on('click', function(){
+		$('body').toggleClass('open-menu')
+	})
+	$('.header__search-button').on('click', function(){
+		$('body').addClass('open-search')
+	})
+	$('.search__close').on('click', function(){
+		$('body').removeClass('open-search')
+	})
+});
