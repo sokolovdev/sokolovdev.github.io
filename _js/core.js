a879af22df10bb4e239773061bbca908bf0258d8
unknown <naraxiss@gmail.com>
2018-02-10 00:19:59 +0300

    article full ready

--- _js/core.js
+++ _js/core.js
@@ -1,11 +1,27 @@
-$(document).ready(function(){
-	$('.header__hamburger').on('click', function(){
-		$('body').toggleClass('open-menu')
-	})
-	$('.header__search-button').on('click', function(){
-		$('body').addClass('open-search')
-	})
-	$('.search__close').on('click', function(){
-		$('body').removeClass('open-search')
-	})
+$(document).ready(function() {
+    $('.header__hamburger').on('click', function() {
+        $('body').toggleClass('open-menu')
+    })
+    $('.header__search-button').on('click', function() {
+        $('body').addClass('open-search')
+    })
+    $('.search__close').on('click', function() {
+        $('body').removeClass('open-search')
+    })
+
+    if ($(window).width() < 1240) {
+        var mySwiperSimilar = new Swiper('.similar__slider', {
+            slidesPerView: 'auto',
+            spaceBetween: 10,
+            freeMode: true
+        });
+
+    } else {
+        if (!(typeof mySwiperSimilar === 'undefined')) {
+            $(window).on('resize', function() {
+                mySwiperSimilar.destroy();
+            })
+        }
+        $(".similar__item").unwrap();
+    }
 });
