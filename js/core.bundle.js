c6c8ca0fab274583cedbfea52bac683319c5b28a
dm <naraxiss@gmail.com>
2018-05-09 01:38:14 +0300

    rdy

--- js/core.bundle.js
+++ js/core.bundle.js
@@ -14,6 +14,58 @@ $('document').ready(function () {
             }
         ]
     });
+    for( i=1; i < 63; i++){
+        $('.popup__body').append('<img data-src=\"/img/photos/' + i + '.jpg\">');
+    }
+
+    function __lI(el) {
+        var imgP = new Image();
+
+        imgP.src = el.dataset.src;
+        imgP.onload = function () {
+            $(el).attr('src', $(el).data('src'));
+            $(el).addClass('loaded');
+
+        };
+
+
+    }
+
+    var image = document.querySelectorAll('.popup img');
+
+
+    $('.callPopup').on('click', function () {
+        $('body').addClass('show-popup');
+        setTimeout(function () {
+            $(image).each(function () {
+                __lI(this);
+            })
+
+        }, 1)
+
+    })
+    $('.popup__close').on('click', function () {
+        $('body').removeClass('show-popup');
+        setTimeout(function () {
+            $('.popup img').attr('src','');
+        }, 300)
+
+    })
+
+
+    $('.header__button').on('click', function () {
+        $(this).toggleClass('active');
+        $('body').toggleClass('show-menu');
+    })
+
+
+    $(".menu__item--main").on("click", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+
 });
 
 
